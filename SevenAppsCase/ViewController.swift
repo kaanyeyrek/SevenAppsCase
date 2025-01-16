c4fd8b10db7872833596d0ced2e7eaf824706fb7
Kaan Yeyrek <kaanyeyrek@icloud.com>
2025-01-16 23:45:18 +0300

    Initial Commit

---
+++ SevenAppsCase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SevenAppsCase
+//
+//  Created by orphex on 16.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
